cb7e71ef4b97f3bdefd80deed75db4eb395a543b
Marnen Laibow-Koser <marnen@marnen.org>
2012-11-21 12:31:16 -0500

    Extract parsing to separate method.

--- lib/parser/comma_delimited.rb
+++ lib/parser/comma_delimited.rb
@@ -3,19 +3,21 @@ require 'parser/base'
 class Parser::CommaDelimited < Parser::Base
   def records
     @records ||= data.collect do |row|
-      record = Record.new
-      fields = row.strip.split /,\s*/
-      record.last_name = fields[0]
-      record.first_name = fields[1]
-      record.gender = fields[2].downcase.to_sym
-      record.favorite_color = fields[3]
-      date = fields[4].split(/\D/).collect &:to_i
-      record.date_of_birth = Date.civil date[2], date[0], date[1]
-      record
+      parse_row row
     end
   end
 
   private
 
-  attr_accessor :data
+  def parse_row(row)
+    record = Record.new
+    fields = row.strip.split /,\s*/
+    record.last_name = fields[0]
+    record.first_name = fields[1]
+    record.gender = fields[2].downcase.to_sym
+    record.favorite_color = fields[3]
+    date = fields[4].split(/\D/).collect &:to_i
+    record.date_of_birth = Date.civil date[2], date[0], date[1]
+    record
+  end
 end
--- lib/parser/pipe_delimited.rb
+++ lib/parser/pipe_delimited.rb
@@ -3,16 +3,20 @@ require 'parser/base'
 class Parser::PipeDelimited < Parser::Base
   def records
     @records ||= data.collect do |row|
-      record = Record.new
-      fields = row.split('|').collect &:strip
-      record.last_name = fields[0]
-      record.first_name = fields[1]
-      record.middle_initial = fields[2]
-      record.gender = GENDER[fields[3]]
-      record.favorite_color = fields[4]
-      date = fields[5].split(/\D/).collect &:to_i
-      record.date_of_birth = Date.civil date[2], date[0], date[1]
-      record
+      parse_row row
     end
+  end
+
+  def parse_row(row)
+    record = Record.new
+    fields = row.split('|').collect &:strip
+    record.last_name = fields[0]
+    record.first_name = fields[1]
+    record.middle_initial = fields[2]
+    record.gender = GENDER[fields[3]]
+    record.favorite_color = fields[4]
+    date = fields[5].split(/\D/).collect &:to_i
+    record.date_of_birth = Date.civil date[2], date[0], date[1]
+    record
   end
 end
